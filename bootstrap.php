276a301746949985acf52d5f01666c4e20ba93d5
Peter Meth <pmeth@rogers.com>
2011-11-17 02:24:47 -0500

    trying to fix spl_autoload case sensitivity issues for unix systems.

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,13 @@
 <?php
 error_reporting(E_ALL);
 
+function classAutoload($class_name) {
+	require_once("classes/$class_name.class.php");
+}
+
+spl_autoload_register('classAutoload');
+
+/*
 // Your custom class dir
 define('CLASS_DIR', 'classes/');
 
@@ -9,6 +16,7 @@ set_include_path(get_include_path() . PATH_SEPARATOR . CLASS_DIR);
 
 spl_autoload_extensions(".class.php"); // comma-separated list
 spl_autoload_register();
+*/
 
 // this was the old way before autoloading.
 //include_once('classes/Rom.class.php');
