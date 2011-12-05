b0841577485f4355f1236833a80a172d86c5a067
Peter Meth <pmeth@rogers.com>
2011-12-04 20:42:30 -0500

    added a csv loader class.  don't have an immediate use for it but it seems like it will come in handy at some point.

---
+++ classes/CSVIterator.php
@@ -0,0 +1,54 @@
+<?php 
+/**
+ * Original from: http://snipplr.com/view/1986/
+ */
+class CSVIterator implements Iterator
+{	
+	const ROW_SIZE = 4096;
+	
+	private $filePointer;
+	private $currentElement;
+	private $rowCounter;
+	private $delimiter;
+	
+	public function __construct( $file, $delimiter = ',' )
+	{
+		$this->filePointer = fopen( $file, 'r' );
+		$this->delimiter   = $delimiter;
+	}
+	
+	public function rewind()
+	{
+		$this->rowCounter = 0;
+		rewind( $this->filePointer );
+	}
+	
+	public function current()
+	{
+		$this->currentElement = fgetcsv( $this->filePointer, self::ROW_SIZE, $this->delimiter );
+		$this->rowCounter++;
+		return $this->currentElement;
+	}
+	
+	public function key()
+	{
+		return $this->rowCounter;
+	}
+	
+	public function next()
+	{
+		return !feof( $this->filePointer );
+	}
+	
+	public function valid()
+	{
+		if( !$this->next() )
+		{
+			fclose( $this->filePointer );
+			return FALSE;
+		}
+		return TRUE;
+	}
+
+} // end class
+?>
