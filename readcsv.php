76038ae1635e29cc12e888fe2ed94d8fb656a3a0
Peter Meth <pmeth@rogers.com>
2011-12-05 00:36:06 -0500

    made some modifications to CSVIterator to allow headings to be loaded from first line and used as array keys on subsequent lines.

--- readcsv.php
+++ readcsv.php
@@ -1,13 +1,16 @@
 <?php
 include('bootstrap.php');
 
+/*
 // Option 1
 //$csv = new parseCSV('../uploads/sample2.txt', null, null, 'Contact First Name = PETER AND Priority = 0001');
-//$csv = new parseCSV('../uploads/sample2.txt');
 
-//foreach($csv->data as $line) {
-//	print_r($line);
-//}
+$csv = new parseCSV('../uploads/sample2.txt');
+
+foreach($csv->data as $line) {
+	print_r($line);
+}
+*/
 
 // Option 2
 $csv = new CSVIterator('../uploads/sample2.txt');
